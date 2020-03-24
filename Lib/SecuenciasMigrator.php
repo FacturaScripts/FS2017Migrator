14f4b0d645deeb766fe9f30ac125f32e96fa9004
NeoRazorX <neorazorx@gmail.com>
2020-03-24 20:10:41 +0100

    Sequences now use gaps.

--- Lib/SecuenciasMigrator.php
+++ Lib/SecuenciasMigrator.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FS2017Migrator plugin for FacturaScripts
- * Copyright (C) 2019 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2019-2020 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -64,6 +64,7 @@ class SecuenciasMigrator extends MigratorBase
                     $secuencia->numero = 1 + $factura->numero;
                     $secuencia->patron = 'FAC{EJE}{SERIE}{NUM}';
                     $secuencia->tipodoc = 'FacturaCliente';
+                    $secuencia->usarhuecos = true;
                     $secuencia->save();
                 }
 
