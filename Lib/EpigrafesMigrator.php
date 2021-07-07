eb70c344de14c9e9fb3a24b2653e49d736da6d26
Carlos García Gómez <neorazorx@gmail.com>
2021-07-07 11:36:20 +0200

    Minor improvement.

--- Lib/EpigrafesMigrator.php
+++ Lib/EpigrafesMigrator.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FS2017Migrator plugin for FacturaScripts
- * Copyright (C) 2019 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2019-2021 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -62,7 +62,7 @@ class EpigrafesMigrator extends MigratorBase
         $sql = "SELECT * FROM co_epigrafes ORDER BY idepigrafe ASC";
         $rows = $this->dataBase->selectLimit($sql, 300, $offset);
         foreach ($rows as $row) {
-            if (!$this->newCuenta($row['codejercicio'], $this->getCodepigrafe($row), $row['codepigrafe'], $row['descripcion'])) {
+            if (false === $this->newCuenta($row['codejercicio'], $this->getCodepigrafe($row), $row['codepigrafe'], $row['descripcion'])) {
                 return false;
             }
 
