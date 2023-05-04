2a6839bba33dfebfc2a78a750e654771666653fe
Carlos Garcia Gomez <neorazorx@gmail.com>
2023-05-04 12:48:12 +0200

    Corregida la migración de usuarios.

--- Lib/UsersMigrator.php
+++ Lib/UsersMigrator.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FS2017Migrator plugin for FacturaScripts
- * Copyright (C) 2019-2022 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2019-2023 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -20,6 +20,7 @@
 namespace FacturaScripts\Plugins\FS2017Migrator\Lib;
 
 use FacturaScripts\Core\Base\DataBase\DataBaseWhere;
+use FacturaScripts\Core\Tools;
 use FacturaScripts\Dinamic\Model\User;
 
 class UsersMigrator extends MigratorBase
@@ -51,8 +52,9 @@ class UsersMigrator extends MigratorBase
 
             // no lo encontramos, lo creamos
             $user->admin = in_array($row['admin'], ['1', 't']);
-            $user->nick = $row['nick'];
             $user->email = $row['email'];
+            $user->newPassword = $user->newPassword2 = Tools::randomString();
+            $user->nick = $row['nick'];
             if (false === $user->save()) {
                 return false;
             }
