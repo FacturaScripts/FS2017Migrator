46430e9f1268232899e9d532e0627269c629b9e0
Carlos Garcia Gomez <neorazorx@gmail.com>
2025-04-29 19:11:02 +0200

    Añadida comprobación de tabla antes de revisar atributos.

--- Lib/AtributosMigrator.php
+++ Lib/AtributosMigrator.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FS2017Migrator plugin for FacturaScripts
- * Copyright (C) 2019-2020 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2019-2025 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -28,9 +28,7 @@ use FacturaScripts\Dinamic\Model\AtributoValor;
  */
 class AtributosMigrator extends MigratorBase
 {
-
     /**
-     *
      * @param int $offset
      *
      * @return bool
--- Lib/ProductosMigrator.php
+++ Lib/ProductosMigrator.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FS2017Migrator plugin for FacturaScripts
- * Copyright (C) 2019-2023 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2019-2025 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -267,6 +267,10 @@ class ProductosMigrator extends MigratorBase
 
     private function removeDuplicatedAttributeValues(): bool
     {
+        if (false === $this->dataBase->tableExists('atributos_valores')) {
+            return true;
+        }
+
         $sql = "SELECT codatributo, valor, COUNT(*) repeticiones FROM atributos_valores"
             . " GROUP BY codatributo, valor HAVING repeticiones > 1;";
         foreach ($this->dataBase->select($sql) as $row) {
